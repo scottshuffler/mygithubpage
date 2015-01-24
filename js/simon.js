9bf5642181b837defaedbf3e2119593634287042
Scott <shuffleres@email.appstate.edu>
2015-01-24 13:15:07 -0500

    Revised

--- js/simon.js
+++ js/simon.js
@@ -1,5 +1,6 @@
-var listening = false;
+var listening;
 var nums = [];
+var score = 0;
 var hexVal;
 var diff;
 var rounds = 0;
@@ -9,13 +10,13 @@ $( document ).ready(function() {
 	$(".difficulty").click( function() {
 		var difficulty = $(this).attr('id');
 		switch( difficulty ) {
-			case "easy":
+			case "Peaceful":
 				getRandom(2);
 				break;
-			case "medium":
+			case "Stressful":
 				getRandom(3);
 				break;
-			case "hard":
+			case "Classic":
 				getRandom(4);
 				break;	
 			case "stop":
@@ -43,13 +44,16 @@ $( document ).ready(function() {
 	});
 });
 
+
+
 function getRandom(difficult)
 {
-	console.log("Get random");
 	diff = difficult;
+	listening = false;
+	document.getElementById('score').innerHTML = 'Score: ' + score;
+	console.log("Should be false " + listening);
 	var i;
 	var rand;
-	
 	for (i=0;i < diff;i++)
 	{
 		(function (i) {
@@ -62,7 +66,8 @@ function getRandom(difficult)
 			setColor(nums[i]);
 			setTimeout(function () {
 				setColor(nums[i]);
-			}, 500);
+				console.log("Should be false " + listening);
+			}, (250));
 		}(i));	
 	}
 	listening = true;
@@ -72,6 +77,7 @@ function getRandom(difficult)
 
 function buttonClick(id)
 {
+	console.log(listening);
 	if (!listening)
 	{
 		return;
@@ -81,48 +87,54 @@ function buttonClick(id)
 		console.log(nums);
 		var id = parseInt(id);
 		var index = nums.indexOf(id);
-		nums.splice(index, 1);
-		console.log(nums);
-		if (nums.length == 0)
-		{
-			getRandom(diff);
-		}
-		else if(index == -1)
+		if(index == -1)
 		{
 			alert("You lost");
 			reload();
 		}
+		console.log(index);
+		nums.splice(index, 1);
+		console.log(nums);
+		if (nums.length == 0)
+		{
+			score = score + 100;
+			document.getElementById('score').innerHTML = 'Score: ' + score;
+			if (diff >= 3)
+			{
+				getRandom(diff + 1);
+			}
+			else
+			{
+				getRandom(diff);
+			}
+		}
+
 	}
 }
+
 function setColor(btn){
 	console.log(btn);
-	listening = false; 
 	var property=document.getElementById(btn);
 	if(property!=null) {
 	   if (window.getComputedStyle(property).backgroundColor == 'rgb(241, 196, 15)') {
 			if (btn == 2 || btn == 5 || btn == 12 || btn == 15)
 				{
 					property.style.backgroundColor='rgb(192, 57, 43)';
-					listening = true; 
 				}
 			else if (btn == 1 || btn == 8 || btn == 11 || btn == 14)
 				{
 					property.style.backgroundColor='rgb(41, 128, 185)';
-					listening = true; 
 				}
 			else if (btn == 3 || btn == 6 || btn == 9 || btn == 16)
 				{
 					property.style.backgroundColor='rgb(39, 174, 96)';
-					listening = true; 
 				}
 			else if (btn == 4 || btn == 7 || btn == 10 || btn == 13){
 					property.style.backgroundColor='rgb(142, 68, 173)';
-					listening = true; 
 				}
 	   }
 		else {
 				property.style.backgroundColor = "#f1c40f";
-				listening = true; 
 		}
 	}
 }
